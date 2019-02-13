bbad1c96da632990ac8c714dbe79436964c4ee50
SebastianGiovagnoni <47358975+SebastianGiovagnoni@users.noreply.github.com>
2019-02-13 12:23:35 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -5,18 +5,20 @@ function mostrar()
 		var mensaje;
 		numerorandom=Math.floor((Math.random() * 10) + 1);
 		console.log(numerorandom);
-	    
-	    if()
-	    {
-	    	
-	    }else
-	    {
-	    	if()
-	    	{
+	    mensaje=""
 
+
+	    if(numerorandom>=9)
+	    {
+	    	alert("exelente");
+	    }else
+	    {   alert("aprobo");
+	    	if(numerorandom<=3)
+	    	{
+	    	alert("vamos, la proxima")
 	    	}else
 	    	{
-
+	    		alert()
 	    	}
 	    }
 
