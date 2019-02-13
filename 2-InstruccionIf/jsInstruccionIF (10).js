702ae8f77f8855bed54e99d4655f568ae2b523ac
SebastianGiovagnoni <47358975+SebastianGiovagnoni@users.noreply.github.com>
2019-02-13 12:21:00 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,24 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+		var numerorandom;
+		var mensaje;
+		numerorandom=Math.floor((Math.random() * 10) + 1);
+		console.log(numerorandom);
+	    
+	    if()
+	    {
+	    	
+	    }else
+	    {
+	    	if()
+	    	{
+
+	    	}else
+	    	{
+
+	    	}
+	    }
+
 
 }//FIN DE LA FUNCIÓN
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,33 @@
 function mostrar()
 {
+ var precio;
+ var producto;
+ var metododepago;
+ var descuentodiez;
+ var descuentoquince;
+ var preciotarjeta;
+ var precioefectivo;
 
+ producto=prompt("ingrese la cantidad de productos que desea comprar");
+ precio=prompt("ingrese el precio");
+ metododepago=prompt("ingrese metodo de pago");
+
+ producto=parseInt(producto);
+ precio=parseInt()
+
+ 
+
+
+
+
+
+
+  
+
+  
+
+
+  
+
+ 
 }
