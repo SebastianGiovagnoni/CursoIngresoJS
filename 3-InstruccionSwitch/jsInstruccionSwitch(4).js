a9c7de693161b86504c813b2e3a86f35cac70035
SebastianGiovagnoni <47358975+SebastianGiovagnoni@users.noreply.github.com>
2019-02-19 12:13:25 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -2,11 +2,33 @@ function mostrar()
 {
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
-
+var mensaje;
 //alert (mesDelAño);
-	
-	
+switch(mesDelAño)
+{
 
+case "Febrero":
+mensaje=("este mes tiene 28 dias");
+break;
 
+case "Enero":
+case "Marzo":
+case "Mayo":
+case "Julio":
+case "Agosto":
+case "Octubre":
+case "Diciembre":
+mensaje=("este mes tiene 31 dias");
+break;
+
+case "Abril":
+case "Junio":
+case "Septiembre":
+case "Noviembre":
+mensaje=("este mes tiene 30 dias")
+break;
+
+}
+alert(mensaje);
 
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch(5).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(5).js
@@ -2,10 +2,42 @@ function mostrar()
 {
 //tomo la edad  
 var laHora = document.getElementById('hora').value;
-
+var mensaje;
 //alert (laHora);
 	
-	
+switch(laHora)
+{
+case "7":
+case "8":
+case "9":
+case "10":
+case "11":
+mensaje=( "Es de mañana.");
+break;
+
+case "1":
+case "2":
+case "3":
+case "4":
+case "5":
+case "6":
+case "12":
+case "13":
+case "14":
+case "15":
+case "16":
+case "17":
+case "18":
+case "19":
+case "20":
+case "21":
+case "22":
+case "23":
+case "24":
+mensaje=("es de tarde. ");
+break;
+}
+alert(mensaje);
 
 
 
