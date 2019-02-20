190bb24c7fcd54a0a77011b6185a90aec56d7cbe
SebastianGiovagnoni <47358975+SebastianGiovagnoni@users.noreply.github.com>
2019-02-20 12:10:19 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (1).js
+++ 5-InstruccionWhile/jsIteraciones (1).js
@@ -1,6 +1,13 @@
+/*toda alteracion tiene un comienzo, un recorrido y una accion que marca su final */
 function mostrar()
 {
-	alert('iteración while');
+	var contador=0;
+	while(contador<10)
+	{
+		contador=contador+1;
+		console.log(contador);
+	}
+	
 
 
 }//FIN DE LA FUNCIÓN
