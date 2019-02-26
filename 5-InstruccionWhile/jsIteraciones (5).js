db00ab7342a98886c0f3810ae800a3daeefb5a33
SebastianGiovagnoni <47358975+SebastianGiovagnoni@users.noreply.github.com>
2019-02-26 11:58:00 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (5).js
+++ 5-InstruccionWhile/jsIteraciones (5).js
@@ -2,6 +2,10 @@ function mostrar()
 {
 
 var sexo = prompt("ingrese f ó m .");
+while(sexo != "f" && sexo != "m")
+{
+	var sexo = prompt("ingrese f ó m .");
+}
 
 
 
--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -3,8 +3,13 @@ function mostrar()
 
 	var contador=0;
 	var acumulador=0;
+	 
 
-
+	while(respuesta)
+	{
+		numero=promedio
+	
+	}
 
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/5;
