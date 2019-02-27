830a2aa6788017bfe5c1a5cc5a44a429d5234aca
SebastianGiovagnoni <47358975+SebastianGiovagnoni@users.noreply.github.com>
2019-02-27 11:58:59 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/ocho.js
@@ -1,4 +1,32 @@
 function mostrar()
 {
+		var contador=0;
+		var acumulador=0;
+		var respuesta="si";
+		var letra;
+		var numero;
+		var numerosPares;
+		var numerosImpares;
+		var cantidadCeros;
+		var promedioNumerosPositivos;
+		var promedioNumerosNegativos;
+		var numMaximo;
+		var numMinimo;
+		var letraMaxima;
+		var letraMinima;	
+
+	while(respues!="no")
+	{
+		numero=prompt("ingrese un numero");
+		numero=parseInt(numero);
+		while(isNaN(numero) == true || numero<-100 || numero >100 )
+		{
+			nota=prompt("error, ingrese numero");
+			numero=parseInt(numero);
+		}
+		letra=prompt("ingrese su letra")
+		while(isNaN(letra) == false)
+
+	}
 
 }
